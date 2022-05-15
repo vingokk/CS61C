3c133a4a3e8a4194a2e1734b38bd8ea11c409911
vingo <wangwenguang98@gmail.com>
2022-05-15 16:37:08 +0800

    Finish cycle detection in the single list

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,14 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
-    return 0;
+    node *tortoise = head; // move in a step of one nodes
+    node *hare = head; //move in a step of two nodes
+    do { 
+        if (!hare) {return 0;}
+        hare = hare -> next;
+	if (!hare) {return 0;}
+	hare = hare -> next;
+	tortoise = tortoise -> next;            
+    } while (tortoise != hare);
+    return 1;
 }
