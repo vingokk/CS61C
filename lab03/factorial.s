3cfad396027f23b4ae74e176837ac599ad6da61f
vingo <wangwenguang98@gmail.com>
2022-06-05 16:55:42 +0800

    Ex03-factorial

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -22,3 +22,21 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    # s0 -> res, s1 -> n
+    addi sp, sp, -8
+    sw ra, 0(sp)
+    sw s0, 4(sp) 
+    addi s0, x0 ,1
+    addi s1, a0, 0
+loop:
+    beq s1, x0, exit
+    mul s0, s0, s1
+    addi s1, s1, -1
+    jal x0, loop
+    
+exit:
+    addi a0, s0, 0
+    lw s0 4(sp)
+    lw ra 0(sp)
+    addi sp, sp, 8
+    jr ra
