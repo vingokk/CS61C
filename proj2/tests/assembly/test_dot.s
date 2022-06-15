24427ad7255f6bad4ab5c76bf8a4c61556b7b0fa
vingo <wangwenguang98@gmail.com>
2022-06-15 17:44:41 +0800

    [Task3.1] Dot Product

--- proj2/tests/assembly/test_dot.s
+++ proj2/tests/assembly/test_dot.s
@@ -6,7 +6,6 @@
 vector0: .word 1 2 3 4 5 6 7 8 9
 vector1: .word 1 2 3 4 5 6 7 8 9
 
-
 .text
 # main function for testing
 main:
@@ -15,19 +14,27 @@ main:
     la s1 vector1
 
     # Set vector attributes
-    
+    addi t0 x0 9
+    addi t1 x0 1
+    addi t2 x0 1
 
 
     # Call dot function
+    mv a0 s0
+    mv a1 s1
+    mv a2 t0
+    mv a3 t1
+    mv a4 t2
+    jal ra dot
+    
+    # Print integer result
+    mv a1 a0
+    jal ra print_int
     
 
-
-    # Print integer result
-
-
-
     # Print newline
-
+    li a1 '\n'
+    jal ra print_char
 
 
     # Exit
